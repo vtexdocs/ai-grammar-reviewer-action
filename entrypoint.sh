10c53d5253da4d5d26d1858c23496da4a86380ab
Henrique Balbino <henrique.balbino@vtex.com>
2026-03-06 21:29:20 -0300

    Update entrypoint to get PR_NUMBER and work with reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@ else
   echo "No issues found, skipping RDJSONL generation."
 fi
 if [ -f suggestions.rdjsonl ]; then
+  if [ -n "$PR_NUMBER" ] && [ -n "$GITHUB_REPOSITORY" ]; then
+    export CI_PULL_REQUEST="https://github.com/$GITHUB_REPOSITORY/pull/$PR_NUMBER"
+  fi
   reviewdog -f=rdjsonl -name="Grammar reviewer" -reporter=github-pr-review -filter-mode=nofilter < suggestions.rdjsonl
 else
   echo "No suggestions found, skipping reviewdog."
